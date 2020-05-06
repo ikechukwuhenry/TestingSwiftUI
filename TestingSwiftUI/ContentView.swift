8b5cb2d03a147e0deb38fb7fd6313c31a34cb5d1
ikechukwuhenry <ikechukwuhenry@ymail.com>
2020-05-06 12:44:48 +0100

    Initial Commit

---
+++ TestingSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestingSwiftUI
+//
+//  Created by ik on 5/6/20.
+//  Copyright © 2020 Mambilla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
