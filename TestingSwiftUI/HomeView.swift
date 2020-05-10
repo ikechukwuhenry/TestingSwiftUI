7652a66b240ce39bd2e79ca32b9a85974c5af5c0
ikechukwuhenry <ikechukwuhenry@ymail.com>
2020-05-10 13:08:40 +0100

    update NavigationView

---
+++ TestingSwiftUI/HomeView.swift
@@ -0,0 +1,119 @@
+//
+//  HomeView.swift
+//  TestingSwiftUI
+//
+//  Created by ik on 5/7/20.
+//  Copyright © 2020 Mambilla. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    @State var showUpdate = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+                
+                AvatarView(showProfile: $showProfile)
+                Button(action: {
+                    self.showUpdate.toggle()
+                }) {
+                    Image(systemName: "bell")
+                        .renderingMode(.original)
+                        .font(.system(size: 16, weight: .medium))
+                        .frame(width: 36, height: 36)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                }   //button
+                    .sheet(isPresented: $showUpdate) {
+                        ContentView()
+                }
+            }
+            .padding(.horizontal)
+            .padding(.leading, 14)
+            .padding(.top, 30)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(sectionData) {item in
+                        GeometryReader { geometry in
+
+                        SectionView(section: item)
+                            .rotation3DEffect(Angle(degrees:
+                                Double(geometry.frame(in: .global).minX - 30) / -20
+                            ), axis: (x: 0, y: 10, z: 0))
+                        }
+                    .frame(width: 275, height: 275)
+                    }
+                    .padding(30)
+                    .padding(.bottom, 30)
+                }
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var section: Section
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack (alignment: .top) {
+                Text(section.title)
+                    .foregroundColor(.white)
+                    .font(.system(size: 24))
+                    .fontWeight(.bold)
+                    .lineLimit(3)
+                    .frame(width: 160, alignment: .leading)
+                Spacer()
+                Image(section.logo)
+            }
+            Text(section.text.uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Spacer()
+            //Image("Card2")
+            section.image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }   // title
+            .padding(.top, 20)
+            .padding(.horizontal, 20)
+            .padding(.leading, 14)
+            .frame(width: 275, height: 275)
+            .background(Color("card1"))
+            .cornerRadius(30)
+            .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
+    }
+}
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionData = [
+    Section(title: "Prototype designs in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))),
+    Section(title: "Build a SwiftUI app", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Background1")), color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))),
+    Section(title: "SwiftUI Advanced", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card2")), color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)))
+]
---
+++ TestingSwiftUI/Utilities.swift
@@ -0,0 +1,12 @@
+//
+//  Utilities.swift
+//  TestingSwiftUI
+//
+//  Created by ik on 5/8/20.
+//  Copyright © 2020 Mambilla. All rights reserved.
+//
+
+import SwiftUI
+
+let screen = UIScreen.main.bounds
+
